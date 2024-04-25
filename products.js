7b21b04250110dff04787caa3c176d4a4094b619
Dyoseb <152150591+Dyoseb@users.noreply.github.com>
2024-04-25 21:33:52 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,73 @@
+import product1 from './assets/1.jpg';
+import product2 from './assets/2.jpg';
+import product3 from './assets/3.jpg';
+import product4 from './assets/4.jpg';
+import product5 from './assets/5.jpg';
+import product6 from './assets/6.jpg';
+import product7 from './assets/7.jpg';
+import product8 from './assets/8.jpg';
+import product9 from './assets/9.jpg';
+import product10 from './assets/10.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Grand Theft Auto',
+        price: 8.50,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'Grand Theft Auto II',
+        price: 9.50,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'Grand Theft Auto III',
+        price: 5.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: 'Grand Theft Auto IV',
+        price: 14.99,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: 'Grand Theft Auto V',
+        price: 10.35,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'Grand Theft Auto: San Andreas',
+        price: 2.18,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'Grand Theft Auto: Vice City',
+        price: 29.99,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'Grand Theft Auto: The trilogy',
+        price: 29.99,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: 'Grand Theft Auto: Liberty City Stories',
+        price: 149.99,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: 'Grand Theft Auto: Online',
+        price: 39.99,
+        productImage: product10,
+    }
+];
